a21fc27f39604ca949ddbda565c72b977b12ceaa
Tuna Figankaplan <swaxztn@gmail.com>
2021-01-31 00:09:24 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> { 
+                new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15,UnitsInStock=15},
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500,UnitsInStock=3},
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500,UnitsInStock=2},
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=200,UnitsInStock=65},
+                new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=175,UnitsInStock=1}
+            };
+        }
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+        
+        public void Add(Product product)
+        {
+            _products.Add(product);
+            Console.WriteLine("{0} added to list.",product.ProductName);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete;
+            productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
+            Console.WriteLine("{0} deleted.",product.ProductName);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate;
+            productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+            Console.WriteLine("{0} updated.",product.ProductName);
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+    }
+}
