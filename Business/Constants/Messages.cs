8619ddd1fee295dde3153540c3a4c121721d2a74
Tuna Figankaplan <swaxztn@gmail.com>
2021-02-18 00:11:36 +0300

    18.02.2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,6 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Product name invalid.";
         public static string MaintenanceTime = "System is busy right now.";
         public static string ProductsListed = "Products listed.";
+        public static string UnitPriceInvalid = "Unit price must be more the zero";
     }
 }
